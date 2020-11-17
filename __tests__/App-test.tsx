54f47ecd8bc0dde022ae14f851e7b307b296c822
MohamedFouadIbrahim <Mohamed_fouad617@yahoo.com>
2020-11-17 19:20:02 +0200

    App-test.tsx

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,10 @@
 
-// import 'react-native';
+import 'react-native';
+import App from '../App';
 import React from 'react';
-// import App from '../App';
 
 // // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 
-const ff =() => {
-    return 2
-}
-it('renders correctly', () => {
-  expect(ff()).toEqual(2)
-});
+
+it('renders correctly', () => { renderer.create(<App />) });
