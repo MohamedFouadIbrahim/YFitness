0c9aaa9366b4bd43ec00b4ff4f88c942035bb882
MohamedFouadIbrahim <Mohamed_fouad617@yahoo.com>
2020-11-24 17:37:24 +0200

    UserHistory

---
+++ containers/User/History/HistoryItem.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import FontedText from '../../../components/FontedText';
+import TranslatedText from '../../../components/TranslatedText';
+import { SystemColors } from '../../../constants/Colors';
+import { PagePadding, PageRadius } from '../../../constants/Styles';
+
+interface RequestsRowProps {
+    gymName: string,
+    historyId: number,
+    onRequestPress?: (gymName: string, historyId: number) => void,
+    requestState: RequestState
+}
+
+export enum RequestState {
+    Canceled = 'Canceled',
+    Accepted = 'Accepted',
+    Holding = 'Holding',
+    Refusing = 'Refusing'
+}
+
+const HistoryRow: React.FC<RequestsRowProps> = (props) => {
+
+    return (
+        <View style={{ marginTop: PagePadding.smallPadding, justifyContent: 'space-between', flexDirection: 'row', alignItems: 'center', backgroundColor: SystemColors.lightDarkBG, padding: 10, borderRadius: 5 }} >
+
+            <FontedText>
+                {`${props.gymName}`}
+            </FontedText>
+
+            <TouchableOpacity
+                disabled={props.requestState == RequestState.Holding ? false : true}
+                onPress={() => { props.onRequestPress && props.onRequestPress(props.gymName, props.historyId) }}
+                style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: SystemColors.darkGray, padding: PageRadius.smallRadius, borderRadius: 5 }}
+            >
+
+                <TranslatedText text={props.requestState} />
+                {/* <View style={{ width: 10, height: 10, borderRadius: 5, backgroundColor: props.available ? 'green' : 'red' }} /> */}
+
+            </TouchableOpacity>
+        </View>
+    )
+}
+
+export default HistoryRow
--- containers/User/History/index.tsx
+++ containers/User/History/index.tsx
@@ -1,28 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { FlatList, View } from "react-native";
 import FontedText from "../../../components/FontedText";
 import RemoteDataContainer from "../../../components/RemoteDataContainer";
 import { SystemColors } from "../../../constants/Colors";
+import { PagePadding } from "../../../constants/Styles";
+import HistoryRow, { RequestState } from "./HistoryItem";
 
 interface UserHistoryProps {
 
 }
 
-interface OneHistory {
-    item: {
-        name?: string,
-        age?: number
-    }
-    index?: number
+const UserHistory: React.FC<UserHistoryProps> = (props) => {
+
+    // const [userRequestHistory] = useState([])
+
+    const userRequestHistory = [
+        {
+            gymName: 'World Gym',
+            historyId: 10,
+            requestState: RequestState.Accepted
+        },
+        {
+            gymName: 'Golds Gym',
+            historyId: 12,
+            requestState: RequestState.Canceled
+        },
+        {
+            gymName: 'Shark Gym',
+            historyId: 11,
+            requestState: RequestState.Holding
+        },
+    ]
+
+    return (
+
+        <FlatList
+            style={{ backgroundColor: SystemColors.darkBG, flexGrow: 1, padding: PagePadding.mediumPadding, paddingBottom: (PagePadding.largePadding * 2) }}
+            data={userRequestHistory}
+            renderItem={({ item: { gymName, historyId, requestState } }) => <HistoryRow gymName={gymName} historyId={historyId} requestState={requestState} />}
+            keyExtractor={(item, index) => String(index)}
+            ListHeaderComponent={() => <FontedText style={{ fontSize: 25, color: 'white', marginBottom: PagePadding.mediumPadding }} >{'Your Requests'}</FontedText>}
+        />
+
+    )
 }
-
-const UserHistory: React.FC<UserHistoryProps> = (props) => (
-
-    <RemoteDataContainer
-        renderItem={(items: OneHistory) => <FontedText>{items.item.name}</FontedText>}
-        keyExtractor={(item, index) => String(index)}
-        contentContainerStyle={{ backgroundColor: SystemColors.darkBG, flexGrow: 1 }}
-
-    />
-
-)
 export default UserHistory
